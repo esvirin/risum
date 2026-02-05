487c1aa130b345e7aba927bfe8e17a9c35bb591c
Evgeny Svirin <esvirin@mail.com>
2026-02-05 19:35:08 +0100

    cookies

--- lib/auth.ts
+++ lib/auth.ts
@@ -24,6 +24,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
     },
     trustHost: true,
+    cookies: {
+        pkceCodeVerifier: {
+            name: `next-auth.pkce.code_verifier`,
+            options: {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production",
+            },
+        },
+    },
     callbacks: {
         async signIn({ user }) {
             // Simply allow sign in if email exists. 
